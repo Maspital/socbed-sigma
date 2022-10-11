3de71d98e566ec1a3dc005502d880f33348dc171
Maspital <philippschlipper@gmail.com>
2022-10-11 16:02:52 +0200

    Rework labeling process

--- src/label_sigma.py
+++ src/label_sigma.py
@@ -1,19 +1,19 @@
 import argparse
 import json
 from pprint import pprint
+import re
 
 from helper import extend_filename
 
 
 def main():
     args = parse_args()
-    with open(args.logfile) as json_file:
-        json_data = json.load(json_file)
-
+    logfile, rules_dict = open_json_files(args.logfile, args.rules_dict)
     rule_counts = {}
-    for sigma_alert in json_data:
+
+    for sigma_alert in logfile:
         rule = sigma_alert["name"]
-        if is_true_positive(sigma_alert, rule):
+        if is_true_positive(sigma_alert, rule, rules_dict):
             rule_counts[rule] = rule_counts.get(rule, 0) + 1
             label_alert(sigma_alert, True)
         else:
@@ -23,55 +23,49 @@ def main():
     print("Rule hits (true positives):")
     pprint(rule_counts)
 
-    new_filename = extend_filename(args.logfile, "LABELED")
-    print(f"Saving labeled dataset to new file {new_filename}.")
-    with open(new_filename, "w", encoding="utf-8") as file:
-        json.dump(json_data, file, ensure_ascii=False, indent=4)
+    save_result(data=logfile,
+                new_filename=extend_filename(args.logfile, "LABELED"))
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Count True Positives')
-    parser.add_argument('logfile', help='JSON log file')
+    parser.add_argument("logfile", help='JSON log file')
+    parser.add_argument("rules_dict", help="dictionary containing rules for labeling")
     return parser.parse_args()
 
 
-def is_true_positive(sigma_alert, rule):
-    command_line = get_command_line(sigma_alert)
-    registry_value = get_registry_value(sigma_alert)
-    if (
-            # c2_mimikatz
-            (rule == 'Meterpreter or Cobalt Strike Getsystem Service Start') or
-            # 2x misc_download_malware
-            (rule == 'Windows PowerShell Web Request' and
-             'meterpreter_bind_tcp.exe' in command_line) or
-            # 1x misc_download_malware
-            (rule == 'Non Interactive PowerShell' and
-             'meterpreter_bind_tcp.exe' in command_line) or
-            # 1x misc_set_autostart
-            (rule == 'Direct Autorun Keys Modification' and
-             'Meterpreter Bind TCP' in command_line) or
-            # 1x misc_set_autostart
-            (rule == 'Autorun Keys Modification' and
-             registry_value == 'Meterpreter Bind TCP')
-    ):
-        return True
-    return False
+def is_true_positive(sigma_alert, rule, rules_dict):
+    label = False
+    target_rule_content = {}
 
+    for rule_name, content in rules_dict.items():
+        if rule_name == rule:
+            target_rule_content = content
+            break
 
-def get_command_line(sigma_alert):
     try:
-        dict_entry = sigma_alert["document"]["data"]["process"]["command_line"]
-        return dict_entry
+        for condition in target_rule_content["conditions"].values():
+            if condition_is_met(sigma_alert, condition):
+                label = True
+                break
     except KeyError:
-        return None
+        return label
+
+    return label
 
 
-def get_registry_value(sigma_alert):
-    try:
-        dict_entry = sigma_alert["document"]["data"]["registry"]["value"]
-        return dict_entry
-    except KeyError:
-        return None
+def condition_is_met(sigma_alert, condition):
+    relevant_dict_entry = condition[0].split(".")
+    desired_content = convert_string_to_regex_pattern(condition[1])
+    actual_content = sigma_alert
+
+    for key in relevant_dict_entry:
+        try:
+            actual_content = actual_content.get(key)
+        except AttributeError:
+            return False
+
+    return desired_content.match(actual_content)
 
 
 def label_alert(sigma_alert, label):
@@ -89,6 +83,33 @@ def rename_fields(sigma_alert):
         sigma_alert["event"] = sigma_alert["document"].pop("data")
     except KeyError:
         pass
+
+
+def convert_string_to_regex_pattern(string):
+    # escape all chars that matter for regex
+    for char in ["\\", ".", "^", "$", "*", "+", "?", "(", ")", "[", "{", "|"]:
+        string = string.replace(char, f"\\{char}")
+
+    # convert keywords to appropriate regex expression
+    string = string.replace("ANY_WORD_CHAR", r"\w+")
+
+    return re.compile(string)
+
+
+def open_json_files(logfile, rules_dict):
+    with open(logfile) as json_file:
+        json_data = json.load(json_file)
+
+    with open(rules_dict) as json_file:
+        json_rules = json.load(json_file)
+
+    return json_data, json_rules
+
+
+def save_result(data, new_filename):
+    print(f"Saving labeled dataset to new file {new_filename}.")
+    with open(new_filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
