8ca869a8e6495a34f941d17dd954371583700102
Maspital <philippschlipper@gmail.com>
2022-10-13 09:56:39 +0200

    Change unit tests to reflect keyword change

--- src/tests/unittests/test_label_sigma.py
+++ src/tests/unittests/test_label_sigma.py
@@ -3,12 +3,12 @@ import re
 
 
 def test_convert_string_to_regex_pattern():
-    string_1 = "cmd.exe /c echo ANY_WORD_CHAR > \\\\.\\pipe\\ANY_WORD_CHAR"
+    string_1 = "cmd.exe /c echo ANY_WORD_CHARS > \\\\.\\pipe\\ANY_WORD_CHARS"
     expected_pattern_1 = re.compile(r"cmd\.exe /c echo \w+ > \\\\\.\\pipe\\\w+")
     pattern_1 = label_sigma.convert_string_to_regex_pattern(string_1)
     assert pattern_1 == expected_pattern_1
 
-    string_2 = ("schtasks  /create /sc once /st 23:59 /tn ANY_WORD_CHAR "
+    string_2 = ("schtasks  /create /sc once /st 23:59 /tn ANY_WORD_CHARS "
                 "/tr C:\\Windows\\meterpreter_bind_tcp.exe /ru BREACH\\client1")
     expected_pattern_2 = re.compile(r"schtasks  /create /sc once /st 23:59 /tn \w+ "
                                     r"/tr C:\\Windows\\meterpreter_bind_tcp\.exe /ru BREACH\\client1")
